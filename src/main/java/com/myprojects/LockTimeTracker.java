97b8e1f2a8df63ce8ac0cb5003649151e9958289
anton.manin <antoxa3030@yandex.ru>
2020-07-01 21:55:14 +0200

    Working Version

---
+++ src/main/java/com/myprojects/LockTimeTracker.java
@@ -0,0 +1,11 @@
+package com.myprojects;
+
+import com.myprojects.tracking.WindowsSessionChangeListener;
+
+public class LockTimeTracker {
+
+    public static void main(String[] args) {
+        new WindowsSessionChangeListener();
+    }
+
+}
